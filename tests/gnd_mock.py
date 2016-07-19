9ba3b7b28d3403f22fbfad121321f76a3cea5777
Kurt <kvonehr@gmail.com>
2016-07-18 20:51:27 -0400

    Updated Serial Port for GS

--- tests/gnd_mock.py
+++ tests/gnd_mock.py
@@ -18,15 +18,15 @@ def receive_loop(in_q):
             msg = in_q.get(block=True, timeout=0.1)
             if msg is not None:
                 if msg_id_to_type[msg.msg_id] == 'attitude' and suppress is False:
-                    print 'Received ATTITUDE packet'
+                    print "Received ATTITUDE packet"
                     print "altitude: {0}" % str(type(msg.altitude))
                     print "heading: {0}" % str(type(msg.heading))
-                    print '---------------------'
+                    print "---------------------"
                 elif msg_id_to_type[msg.msg_id] == 'detection' and suppress is False:
-                    print 'Received DETECTION packet'
+                    print "Received DETECTION packet"
                     print "snr: {0}" % str(msg.snr)
                     print "heading: {0}" % str(msg.heading)
-                    print '---------------------'
+                    print "---------------------"
 
         except Queue.Empty as e:
             continue
