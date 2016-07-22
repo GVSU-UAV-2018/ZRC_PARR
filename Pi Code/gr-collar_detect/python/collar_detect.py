4aa3fd6fad534463eb226ebb43db005e9e366405
Kurt <kvonehr@gmail.com>
2016-07-22 17:08:36 -0400

    Updated Serial Port for GS

--- Pi Code/gr-collar_detect/python/collar_detect.py
+++ Pi Code/gr-collar_detect/python/collar_detect.py
@@ -71,8 +71,7 @@ class collar_detect(gr.sync_block):
             i = 0
 
         if(noise_var > 5*var_avg):
-            #pub.sendMessage('detection')
-            #print numpy.max(noise_norm)
+            pub.sendMessage('detection')
+            print numpy.max(noise_norm)
 
         return len(input_items[0])
-
