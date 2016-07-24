4d31be31fcd4615a3e907286f986e7eb7127b994
Kurt <kvonehr@gmail.com>
2016-07-24 12:57:54 -0400

    Updated Serial Port for GS

--- Pi Code/Serial_CRC.py
+++ Pi Code/Serial_CRC.py
@@ -12,14 +12,15 @@ PROTOCOL_DLE = '\x90'
 
 # configure the serial connections (the parameters differs on the device you are connecting to)
 ser = serial.Serial(
-    port= '/dev/ttyAMA0',
+    port='/dev/ttyAMA0',
     baudrate=57600,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS,
-    timeout = 0
+    timeout=0
 )
 
 ser.isOpen()
+
 
 def send_serial(direction, data_type, data):
     """ Given the data, builds a message for
@@ -36,8 +37,8 @@ def send_serial(direction, data_type, data):
         direction=direction,
         data_type=data_type,
         data=data,
-	scanning = 0,
-        crc = 0))
+        scanning=0,
+        crc=0))
 
     # Compute the CRC field and append it to the
     # message instead of the empty CRC specified
@@ -51,19 +52,19 @@ def send_serial(direction, data_type, data):
     msg = msg_without_crc + msg_crc
 
     pw = ProtocolWrapper(
-            header=PROTOCOL_HEADER,
-            footer=PROTOCOL_FOOTER,
-            dle=PROTOCOL_DLE)
-
+        header=PROTOCOL_HEADER,
+        footer=PROTOCOL_FOOTER,
+        dle=PROTOCOL_DLE)
 
     ser.write(pw.wrap(msg))
+
 
 def receive_serial(tb):
     # Sample: receiving a message
     pw = ProtocolWrapper(
-            header=PROTOCOL_HEADER,
-            footer=PROTOCOL_FOOTER,
-            dle=PROTOCOL_DLE)
+        header=PROTOCOL_HEADER,
+        footer=PROTOCOL_FOOTER,
+        dle=PROTOCOL_DLE)
     msg = ''
 
     time.sleep(2)
@@ -83,11 +84,12 @@ def receive_serial(tb):
                 if rec_crc != calc_crc:
                     print 'Error: CRC mismatch'
                 else:
-		    tb.update_vars(message_format.parse(rec_msg))	
-                    #print "Data Received"
+                    tb.update_vars(message_format.parse(rec_msg))
+                    print "Data Received"
                 msg = ''
         except:
             pass
+
 
 def ser_close():
     ser.close()
