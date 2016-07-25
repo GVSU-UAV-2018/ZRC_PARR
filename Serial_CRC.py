31c24bf1aa56de5d64eab8bd897566ca3d838830
Kurt <kvonehr@gmail.com>
2016-07-25 19:42:01 -0400

    Updated Serial Port for GS

--- Serial_CRC.py
+++ Serial_CRC.py
@@ -24,6 +24,7 @@ closing = False
 
 ser.isOpen()
 
+
 def send_serial(direction, data_type, data, scanning):
     """ Given the data, builds a message for
         transmittion, computing the CRC and packing
@@ -38,8 +39,8 @@ def send_serial(direction, data_type, data, scanning):
         direction=direction,
         data_type=data_type,
         data=data,
-        scanning = scanning,
-        crc = 0))
+        scanning=scanning,
+        crc=0))
 
     # Compute the CRC field and append it to the
     # message instead of the empty CRC specified
@@ -53,47 +54,47 @@ def send_serial(direction, data_type, data, scanning):
     msg = msg_without_crc + msg_crc
 
     pw = ProtocolWrapper(
-            header=PROTOCOL_HEADER,
-            footer=PROTOCOL_FOOTER,
-            dle=PROTOCOL_DLE)
-
+        header=PROTOCOL_HEADER,
+        footer=PROTOCOL_FOOTER,
+        dle=PROTOCOL_DLE)
 
     ser.write(pw.wrap(msg))
 
-def receive_serial(panel):        pass
-    # Sample: receiving a message
-    #
-    ser.isOpen()
-    pw = ProtocolWrapper(
-            header=PROTOCOL_HEADER,
-            footer=PROTOCOL_FOOTER,
-            dle=PROTOCOL_DLE)
-    msg = ''
 
-    while closing == False:
-        byte = ser.read()
-        status = map(pw.input, byte)
-        try:
-            if status[-1] == ProtocolStatus.MSG_OK:
-                rec_msg = pw.last_message
-                # Parse the received CRC into a 32-bit integer
-                #
-                rec_crc = message_crc.parse(rec_msg[-4:]).crc
-                # Compute the CRC on the message
-                #
-                calc_crc = crc32(rec_msg[:-4])
-                if rec_crc != calc_crc:
-                    print 'Error: CRC mismatch'
-                else:
-                    rcvd_msg = message_format.parse(rec_msg)
-                    print "Data Received"
-                    panel.update_on_receive(rcvd_msg)
-                msg = ''
-        except Exception as ex:
-            import sys
-            print str(ex)
-            print "Unexpected error:", sys.exc_info()[0]
-            raise
+def receive_serial():        pass  # Sample: receiving a message
+ser.isOpen()
+pw = ProtocolWrapper(
+    header=PROTOCOL_HEADER,
+    footer=PROTOCOL_FOOTER,
+    dle=PROTOCOL_DLE)
+msg = ''
+
+while closing == False:
+    byte = ser.read()
+    status = map(pw.input, byte)
+    try:
+        if status[-1] == ProtocolStatus.MSG_OK:
+            rec_msg = pw.last_message
+            # Parse the received CRC into a 32-bit integer
+            #
+            rec_crc = message_crc.parse(rec_msg[-4:]).crc
+            # Compute the CRC on the message
+            #
+            calc_crc = crc32(rec_msg[:-4])
+            if rec_crc != calc_crc:
+                print 'Error: CRC mismatch'
+            else:
+                rcvd_msg = message_format.parse(rec_msg)
+                print "Data Received"
+                panel.update_on_receive(rcvd_msg)
+            msg = ''
+    except Exception as ex:
+        import sys
+
+        print str(ex)
+        print "Unexpected error:", sys.exc_info()[0]
+        raise
+
 
 def ser_close():
     closing = True
