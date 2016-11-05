16aae61c3405267012c7059a0ab6891b8bdffd63
Seth Hilaski <seth.hilaski@gmail.com>
2016-11-04 20:59:55 -0400

    Added most recent collar_detect

--- Pi Code/gr-collar_detect/python/collar_detect.py
+++ Pi Code/gr-collar_detect/python/collar_detect.py
@@ -19,58 +19,68 @@
 # Boston, MA 02110-1301, USA.
 # 
 
-import scipy
 import numpy
-from scipy import fftpack
-from scipy import stats
-from scipy import signal
 from gnuradio import gr
+from pubsub import pub
 
 i = 0
 var_avg = 0.0
-var_avg_temp = 0.0
+var_avg_temp = []
 prev_time = 0.0
 collar_offset = 3000
 sample_freq_decim = 16000.0
 collar_bandwidth = 1000.0
-max_bin = int(((collar_offset+collar_bandwidth/2)/sample_freq_decim) * 512)
-min_bin = int(((collar_offset-collar_bandwidth/2)/sample_freq_decim) * 512)
+#max_bin = int(((collar_offset + collar_bandwidth / 2.0) / sample_freq_decim) * 512.0)
+#min_bin = int(((collar_offset - collar_bandwidth / 2.0) / sample_freq_decim) * 512.0)
 
 
 class collar_detect(gr.sync_block):
     """
     docstring for block collar_detect
     """
+
     def __init__(self):
         gr.sync_block.__init__(self,
-            name="collar_detect",
-            in_sig=[(numpy.float32,512)],
-            out_sig=None)
-
+                               name="collar_detect",
+                               in_sig=[(numpy.float32, 512)],
+                               out_sig=None)
+        self.snr_threshold = 6.0
 
     def work(self, input_items, output_items):
         global var_avg
-	global min_bin
-	global max_bin
-	global average_mag
-	global i
-	global var_avg_temp
-	in0 = input_items[0]
-	
-	noise_mean = numpy.mean(in0[0][min_bin:max_bin])
-	noise_norm = numpy.asarray(in0[0][min_bin:max_bin]) - noise_mean
-	noise_var = numpy.var(noise_norm)
-	
-	if(i<31):
-		var_avg_temp = var_avg_temp + noise_var
-		i = i + 1	
-	else:
-		var_avg = var_avg_temp / 31
-		var_avg_temp = 0.0
-		i = 0
-	
-	if(noise_var > 5*var_avg):
-		print numpy.max(noise_norm)
-	
-	return len(input_items[0])
+        #global min_bin
+        #global max_bin
+        global i
+        global var_avg_temp
+        in0 = input_items[0]
+
+        noise_mean = numpy.mean(in0[0])
+        #noise_norm = numpy.asarray(in0[0][min_bin:max_bin]) - noise_mean
+        #noise_var = numpy.var(noise_norm)
+
+        noise_var = numpy.var(in0[0])
+
+        # at 16kHz and 512 samples, 31 windows is approximately .992 seconds. The pulse is approximately 1.2s.
+        # because the pulse will be included in the "noise floor" calculation the majority of the time
+        # and the pulse is approximately 2ms, and each window of 512 samples is 3.2ms, and the pulse may
+        # overlap two windows, the two largest var_avg_temp values should be destroyed because they likely
+        # are contributed from the pulse and do not truly represent the noise floor.
+
+        if i < 310:
+            var_avg_temp.append(noise_mean)
+            i += 1
+        else:
+            # remove largest two values from var_avg_temp before averaging
+            var_avg_temp.remove(max(var_avg_temp))
+            var_avg_temp.remove(max(var_avg_temp))
+            var_avg = numpy.mean(var_avg_temp)
+            var_avg_temp = []
+            i = 0
+
+        if noise_mean > self.snr_threshold * var_avg:
+            detected_pulse = noise_mean / var_avg
+            pub.sendMessage('detection', magnitude=detected_pulse)
+            # print numpy.max(noise_norm)
+
+        return len(input_items[0])
 
