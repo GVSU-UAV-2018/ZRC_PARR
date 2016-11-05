93fc8df79c47e331ae15746f723fbab45cb16be1
Seth Hilaski <seth.hilaski@gmail.com>
2016-11-04 21:06:44 -0400

    Restored old gr-collar_detect folder

---
+++ Pi Code/gr-collar_detect/build/include/collar_detect/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/RDF System/gr-collar_detect/include/collar_detect
+# Build directory: /home/pi/RDF System/gr-collar_detect/build/include/collar_detect
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
